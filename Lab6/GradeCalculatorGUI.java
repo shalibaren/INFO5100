0563b605feff5d9d56ebba9c3ac3d1db16673e66
Ifeng Wu <steven.eliteam@gmail.com>
2022-11-12 10:45:08 -0800

    Update Lab6

--- Lab6/GradeCalculatorGUI.java
+++ Lab6/GradeCalculatorGUI.java
@@ -10,6 +10,7 @@ class GradeCalculatorGUI extends JFrame implements ActionListener {
     static JLabel earnedPoints_label;
     static JLabel percentageOfClass_label;
     static JLabel weightedScore_label;
+    static JLabel showScore_label;
 
     // JTextField
     static JTextField totalAssignmentPoints_textField;
@@ -28,46 +29,49 @@ class GradeCalculatorGUI extends JFrame implements ActionListener {
     public void calculator()
     {
         // create a new frame to store text field and button
-        frame = new JFrame("Lab6 Assignment");
+        frame = new JFrame("Lab6 Assignment - Grade Calculator with GUI");
 
         // create a label to display text
         totalAssignmentPoints_label = new JLabel("Total Assignment Points");
         earnedPoints_label = new JLabel("Earned Points");
         percentageOfClass_label = new JLabel("Percentage of class");
-        weightedScore_label = new JLabel("Weighted Score");
+        weightedScore_label = new JLabel("Weighted Score:");
+        showScore_label = new JLabel("       ");
 
         // create a object of JTextField with 16 columns and a given initial text
-        totalAssignmentPoints_textField = new JTextField("Enter Total Assignment Points", 16);
-        earnedPoints_textField = new JTextField("Enter Earned Points", 16);
-        percentageOfClass_textField = new JTextField("Enter Percentage Of class", 16);
+        totalAssignmentPoints_textField = new JTextField("Enter Points", 16);
+        earnedPoints_textField = new JTextField("Enter Points", 16);
+        percentageOfClass_textField = new JTextField("Enter Percentage", 16);
+
 
         // create a new button
-        button = new JButton("Click to calculate Score");
+        button = new JButton("Click to Calculate");
 
         // create a object of the text class
-        GradeCalculatorGUI gradeCal = new GradeCalculatorGUI();
+        GradeCalculatorGUI gradeCalculate = new GradeCalculatorGUI();
 
         // addActionListener to button
-        button.addActionListener(gradeCal);
+        button.addActionListener(gradeCalculate);
 
         // create a panel to add buttons and textfield
-        JPanel p = new JPanel();
+        JPanel jp = new JPanel();
 
-        // add buttons and textfield to panel
-        p.add(totalAssignmentPoints_label);
-        p.add(totalAssignmentPoints_textField);
-        p.add(earnedPoints_label);
-        p.add(earnedPoints_textField);
-        p.add(percentageOfClass_label);
-        p.add(percentageOfClass_textField);
-        p.add(button);
-        p.add(weightedScore_label);
+        // add buttons and textfield
+        jp.add(totalAssignmentPoints_label);
+        jp.add(totalAssignmentPoints_textField);
+        jp.add(earnedPoints_label);
+        jp.add(earnedPoints_textField);
+        jp.add(percentageOfClass_label);
+        jp.add(percentageOfClass_textField);
+        jp.add(button);
+        jp.add(weightedScore_label);
+        jp.add(showScore_label);
 
-        // add panel to frame
-        frame.add(p);
+        // add panel
+        frame.add(jp);
 
-        // set the size of frame
-        frame.setSize(400, 400);
+        // set the size
+        frame.setSize(400, 600);
 
         frame.setVisible(true);
     }
@@ -76,20 +80,20 @@ class GradeCalculatorGUI extends JFrame implements ActionListener {
         public void actionPerformed(ActionEvent e)
         {
             String s = e.getActionCommand();
-            if (s.equals("Click to calculate Score")) {
+            if (s.equals("Click to Calculate")) {
                 // set the text of the label to the text of the field
                 double pointTotal = Double.parseDouble(totalAssignmentPoints_textField.getText());
                 double earnPoints = Double.parseDouble(earnedPoints_textField.getText());
-                double percentage = Double.parseDouble(percentageOfClass_textField.getText());
+                double weight = Double.parseDouble(percentageOfClass_textField.getText());
     
                 // create weightedGrade object
                 WeightedGrade wg = new WeightedGrade();
                 wg.setPointTotal(pointTotal);
                 wg.setEarnedPoints(earnPoints);
-                wg.setAssignmentPercentage(percentage);
+                wg.setAssignmentWeight(weight);
     
                 // set the text of field to blank
-                weightedScore_label.setText(String.format("%.1f", wg.TotalWeightedGrade()));
+                showScore_label.setText(String.format("%.1f", wg.TotalWeightedGrade()));
             }
         }
 }
--- Lab6/WeightedGrade.java
+++ Lab6/WeightedGrade.java
@@ -3,35 +3,35 @@ package Lab6;
 public class WeightedGrade {
     private double pointTotal;
     private double earnedPoints;
-    private double assignmentPercentage;
+    private double weight;
 
     public double getPointTotal() {
         return pointTotal;
-    }
-
-    public void setPointTotal(double pointTotal) {
-        this.pointTotal = pointTotal;
     }
 
     public double getEarnedPoints() {
         return earnedPoints;
     }
 
+    public double getAssignmentWeight() {
+        return weight;
+    }
+
+    public void setPointTotal(double pointTotal) {
+        this.pointTotal = pointTotal;
+    }
+
     public void setEarnedPoints(double earnedPoints) {
         this.earnedPoints = earnedPoints;
     }
 
-    public double getAssignmentPercentage() {
-        return assignmentPercentage;
-    }
-
-    public void setAssignmentPercentage(double assignmentPercentage) {
-        this.assignmentPercentage = assignmentPercentage;
+    public void setAssignmentWeight(double weight) {
+        this.weight = weight;
     }
 
     /// Calculate Total Weighted Grade
     public double TotalWeightedGrade() {
-        return (earnedPoints / pointTotal) * assignmentPercentage;
+        return (earnedPoints / pointTotal) * weight;
     }
 
 }
